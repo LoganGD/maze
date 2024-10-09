4f4bb29718702a82754d55a4e9bb598943c6a023
LoganGD <logangd2006@gmail.com>
2024-10-09 13:11:13 -0600

    maze 2.5

--- main.py
+++ main.py
@@ -58,15 +58,12 @@ class Cell:
         else:
             line.draw(self._win.canvas, "#d9d9d9")
     
-    def draw_move(self, to_cell, color=None):
+    def draw_move(self, to_cell, color):
         move = Line(
             Point((self._x1+self._x2)/2,(self._y1+self._y2)/2),
             Point((to_cell._x1+to_cell._x2)/2,(to_cell._y1+to_cell._y2)/2)
         )
-        if color != None:
-            move.draw(self._win.canvas, color)
-        else:
-            move.draw(self._win.canvas,"red")
+        move.draw(self._win.canvas, color)
 
     def draw_center(self, color):
         self._win.canvas.create_oval((self._x1+self._x2)/2-3,(self._y1+self._y2)/2-3,(self._x1+self._x2)/2+3,(self._y1+self._y2)/2+3, fill = color, outline = color)
@@ -82,6 +79,7 @@ class Maze:
         cell_size_y,
         win,
         speed,
+        second,
         seed = None
     ):
         self.x1 = x1
@@ -91,6 +89,7 @@ class Maze:
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
+        self.second = second
         if seed != None:
             random.seed(seed)
         self.set_speed(speed)
@@ -100,7 +99,11 @@ class Maze:
         self._reset_cells_visited()
         self.posx = 0
         self.posy = 0
+        self.posx2 = 0
+        self.posy2 = 0
         self.draw_pos()
+        if self.second:
+            self.draw_pos2()
 
     def _create_cells(self):
         self._cells = []    
@@ -195,64 +198,80 @@ class Maze:
             for j in range(self.num_rows):
                 self._cells[i][j].visited = False
     
-    def solve(self, trail_color):
-        return self._solve_r(0, 0, trail_color)
+    def solve(self, color, trail_color):
+        self.erase_pos()
+        self.erase_pos2()
+        return self._solve_r(0, 0, color, trail_color)
     
-    def _solve_r(self, i, j, trail_color):
+    def _solve_r(self, i, j, color, trail_color):
         self._animate()
         self._cells[i][j].visited = True
         if i == self.num_cols - 1 and j == self.num_rows -1:
             return True
         if i < self.num_cols - 1 and self._cells[i+1][j].visited == False and self._cells[i][j].has_right_wall == False:
-            self._cells[i][j].draw_move(self._cells[i+1][j])
-            if self._solve_r(i+1, j, trail_color) == True:
+            self._cells[i][j].draw_move(self._cells[i+1][j], color)
+            if self._solve_r(i+1, j, color, trail_color) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i+1][j], trail_color)
         if j < self.num_rows - 1 and self._cells[i][j+1].visited == False and self._cells[i][j].has_bottom_wall == False:
-            self._cells[i][j].draw_move(self._cells[i][j+1])
-            if self._solve_r(i, j+1, trail_color) == True:
+            self._cells[i][j].draw_move(self._cells[i][j+1], color)
+            if self._solve_r(i, j+1, color, trail_color) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i][j+1], trail_color)
         if 0 < j and self._cells[i][j-1].visited == False and self._cells[i][j].has_top_wall == False:
-            self._cells[i][j].draw_move(self._cells[i][j-1])
-            if self._solve_r(i, j-1, trail_color) == True:
+            self._cells[i][j].draw_move(self._cells[i][j-1], color)
+            if self._solve_r(i, j-1, color, trail_color) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i][j-1], trail_color)
         if 0 < i and self._cells[i-1][j].visited == False and self._cells[i][j].has_left_wall == False:
-            self._cells[i][j].draw_move(self._cells[i-1][j])
-            if self._solve_r(i-1, j, trail_color) == True:
+            self._cells[i][j].draw_move(self._cells[i-1][j], color)
+            if self._solve_r(i-1, j, color, trail_color) == True:
                 return True
             self._cells[i][j].draw_move(self._cells[i-1][j], trail_color)
         
         return False
     
     def move(self, dir, trail_color):
+        self.erase_pos()
+        self.erase_pos2()
         if dir == "Right" and self.posx < self.num_cols - 1 and self._cells[self.posx][self.posy].has_right_wall == False:
-            self.erase_pos()
             self.posx += 1
-            self.draw_pos()
         if dir == "Down" and self.posy < self.num_rows - 1 and self._cells[self.posx][self.posy].has_bottom_wall == False:
-            self.erase_pos()
             self.posy += 1
-            self.draw_pos()
         if dir == "Up" and 0 < self.posy and self._cells[self.posx][self.posy].has_top_wall == False:
-            self.erase_pos()
             self.posy -= 1
-            self.draw_pos()
         if dir == "Left" and 0 < self.posx and self._cells[self.posx][self.posy].has_left_wall == False:
-            self.erase_pos()
             self.posx -= 1
-            self.draw_pos()
+        if self.second and dir == "d" and self.posx2 < self.num_cols - 1 and self._cells[self.posx2][self.posy2].has_right_wall == False:
+            self.posx2 += 1
+        if self.second and dir == "s" and self.posy2 < self.num_rows - 1 and self._cells[self.posx2][self.posy2].has_bottom_wall == False:
+            self.posy2 += 1
+        if self.second and dir == "w" and 0 < self.posy2 and self._cells[self.posx2][self.posy2].has_top_wall == False:
+            self.posy2 -= 1
+        if self.second and dir == "a" and 0 < self.posx2 and self._cells[self.posx2][self.posy2].has_left_wall == False:
+            self.posx2 -= 1
+        self.draw_pos()
+        if self.second:
+            self.draw_pos2()
         self._animate()
         if self.posx == self.num_cols - 1 and self.posy == self.num_rows -1:
             self.set_speed(0)
-            self.solve(trail_color)
+            self.solve("red", trail_color)
+        if self.posx2 == self.num_cols - 1 and self.posy2 == self.num_rows -1:
+            self.set_speed(0)
+            self.solve("blue", trail_color)
 
     def draw_pos(self):
         self._cells[self.posx][self.posy].draw_center("red")
 
     def erase_pos(self):
         self._cells[self.posx][self.posy].draw_center("#d9d9d9")
+    
+    def draw_pos2(self):
+        self._cells[self.posx2][self.posy2].draw_center("blue")
+
+    def erase_pos2(self):
+        self._cells[self.posx2][self.posy2].draw_center("#d9d9d9")
     
 class GUI:
     def __init__(self, width, height):
@@ -263,16 +282,19 @@ class GUI:
         self.active = False
         self.maze = None
         self.trail_color = "#d9d9d9"
-        
+        self.second = False
+
         tab = "               "
         tk.Label(text=f"Number of rows: 4-50{tab}Number columns: 4-90{tab}Speeds: 0 for off, 1 for very slow, 2 for slow, 3 for medium, 4 for fast, 5 for very fast").pack(pady=10)
         self.rows = self.create_input("Number of rows", "10")
         self.cols = self.create_input("Number of columns", "18")
         self.speed = self.create_input("Speed","3")
         self.trail = tk.IntVar()
+        self.players = tk.IntVar()
         tk.Checkbutton(text="Show trail", variable=self.trail, command=self.change_trail).pack(side=tk.LEFT, padx=10, pady=10)
-        tk.Button(text="Solve", command=self.solve).pack(side=tk.RIGHT, padx=10, pady=10)
-        tk.Button(text="Generate", command=self.generate).pack(side=tk.RIGHT, padx=10, pady=10)
+        tk.Checkbutton(text="Second player", variable=self.players, command=self.change_players).pack(side=tk.LEFT, padx=10, pady=10)
+        self.button1 = tk.Button(text="Solve", command=self.solve).pack(side=tk.RIGHT, padx=10, pady=10)
+        self.button2 = tk.Button(text="Generate", command=self.generate).pack(side=tk.RIGHT, padx=10, pady=10)
 
     def create_input(self, text, default):
         tk.Label(text=text).pack(side=tk.LEFT, padx=10, pady=10)
@@ -287,8 +309,7 @@ class GUI:
             try:
                 speed = int(self.speed.get())
                 self.maze.set_speed(speed)
-                self.maze.erase_pos()
-                self.maze.solve(self.trail_color)
+                self.maze.solve("green", self.trail_color)
                 self.maze = None
                 self.active = False
             except:
@@ -304,7 +325,8 @@ class GUI:
                 if num_rows < 4 or num_rows > 50 or num_cols < 4 or num_cols > 90 or speed < 0 or speed > 5:
                     raise ValueError
                 self.win.canvas.delete("all")
-                self.maze = Maze(20, 20, num_rows, num_cols, (self.width-40)/num_cols, (self.height-40)/num_rows, self.win, speed)
+                self.maze = Maze(20, 20, num_rows, num_cols, (self.width-40)/num_cols, (self.height-40)/num_rows, self.win, speed, self.second)
+                self.maze.second = self.second
                 self.active = False
             except:
                 self.active = False
@@ -318,6 +340,18 @@ class GUI:
     def on_key_press(self, event):
         if not self.active and self.maze != None:
             self.maze.move(event.keysym, self.trail_color)
+
+    def change_players(self):
+        if self.second:
+            self.second = False
+            if not self.active and self.maze != None:
+                self.maze.second = False
+                self.maze.erase_pos2()
+        else:
+            self.second = True
+            if not self.active and self.maze != None:
+                self.maze.second = True
+                self.maze.draw_pos2()
 
 def main():
     sys.setrecursionlimit(4500)
